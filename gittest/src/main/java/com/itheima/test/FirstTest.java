1a44f0de61c5cb39d2c7fe96b2437189ac11e7f4
xiaowangba <1765014157@qq.com>
2019-06-24 18:37:27 +0800

    第一次提交

---
+++ gittest/src/main/java/com/itheima/test/FirstTest.java
@@ -0,0 +1,13 @@
+package com.itheima.test;
+
+public class FirstTest {
+    public static void main(String[] args) {
+        System.out.println("我是你爸爸");
+        System.out.println("我是你爸爸");
+        System.out.println("我是你爸爸");
+        System.out.println("我是你爸爸");
+        System.out.println("我是你爸爸");
+        System.out.println("我是你爸爸");
+        System.out.println("我是你爸爸");
+    }
+}
